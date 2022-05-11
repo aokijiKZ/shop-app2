dfd17f43f00590e5e1458a879a4f0e0d1ea6e70b
aunyamanee99 <aunyamanee@kkumail.com>
2022-05-11 14:15:30 +0700

    Created project.

---
+++ database/migrations/2022_05_11_065205_create_product_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('productID');
+            $table->unsignedBigInteger('sizeID');
+            $table->unsignedBigInteger('genderID');
+            $table->float('price')->default(0.0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
